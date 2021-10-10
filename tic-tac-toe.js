6db99540c3d3c80c196b21312c571e52c9aa8612
Riverine-jpg <nateb2001@gmail.com>
2021-10-10 03:02:15 -0500

    Updated JS 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,39 @@
+let check = 0;
+let arr = new Array();
 
 (function(window,document,undefined){
     window.onload = init;
     function init(){
         var size = 9;
+        
         var board = document.getElementById('board');
+        var spaces = board.children;
         for(var i = 0;i<size;i++){
-            board.children[i].classList.add("square");
-            console.log(board.children[i]);
+            spaces[i].classList.add("square");
+            spaces[i].id = i
+            spaces[i].onclick = function() {play(this)};
+            console.log(spaces[i]);
         }
+        
     }
 })(window,document,undefined)
 
+function play(el){
+    if(el.childNodes.length == 0){
+        switch(check){
+            case 0:
+                el.classList.add("X");
+                el.textContent = "X";
+                check = 1;
+                break;
+            case 1:
+                el.classList.add("O");
+                el.textContent = "O";
+                check = 0;
+                break;
+        }
+        console.log("Ah yes bread");
+    }
+}
 
 
